451f66c2cf509935998e325617368962a4c67cc2
Connor <connor@admin.com>
2018-09-19 13:42:05 -0500

    the -s option is working and forking proper number of children at once
    nextly, shared memory

--- main.c
+++ main.c
@@ -7,23 +7,37 @@
 #include <sys/wait.h>
 
 // performs forking and work to be done
-void forkerMaster (int n) {
-    pid_t childpid = 0;
-    pid_t pid;
+void forkerMaster (int n, int s) {
+    pid_t childpid = 0, wpid;
     int counter1;
+    int status;
 
-    // create forks
-    for (counter1 = 1; counter1 < n; counter1++) {
-        if ((childpid = fork()))
-            break;
+
+    for(counter1 = 0; counter1 < s; counter1++){
+        if (childpid = fork() == 0)
+            execl ("/home/crbaniak/Documents/umslClasses/fall18/4760_OS/code/ass2/Worker",
+                   "Worker", NULL);
     }
 
-    // exec call to 'Worker' executable
-    execl ("/home/crbaniak/Documents/umslClasses/fall18/4760_OS/code/ass2/Worker",
-           "Worker", NULL);
+    while (counter1 < n) {
+        wait(&status);
+        if (childpid = fork() == 0)
+            execl ("/home/crbaniak/Documents/umslClasses/fall18/4760_OS/code/ass2/Worker",
+                   "Worker", NULL);
+        counter1++;
+    }
+
+//    // create forks
+//    for (counter1 = 0; counter1 < n; counter1++) {
+//        if (childpid = fork())
+//            execl ("/home/crbaniak/Documents/umslClasses/fall18/4760_OS/code/ass2/Worker",
+//                   "Worker", NULL);
+//    }
 
     //*** children never come back to here.
 
+
+    wait(NULL);
 
     fprintf(stderr, "i:%d, process ID:%ld, parent ID: %ld, child ID:%ld\n",
             counter1, (long)getpid(), (long)getppid(), (long)childpid);
@@ -73,12 +87,13 @@ int main (int argc, char **argv) {
                              "Unknown option character `\\x%x'.\n",
                              optopt);
                 return 1;
+
             default:
-                abort ();
+                abort();
         }
 
         if(isForkCalled == 1){
-            forkerMaster(n);
+            forkerMaster(n,s);
         }
 
         sleep(1);
@@ -86,74 +101,3 @@ int main (int argc, char **argv) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-//
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <sys/types.h>
-//#include <unistd.h>
-//
-//
-//int main(void)
-//{
-//    char *my_args[5];
-//    pid_t pid;
-//
-//    my_args[0] = "child.exe";
-//    my_args[1] = "arg1";
-//    my_args[2] = "arg2";
-//    my_args[3] = NULL;
-//
-//    puts ("fork()ing");
-//
-//    switch ((pid = fork()))
-//    {
-//        case -1:
-//            /* Fork() has failed */
-//            perror ("fork");
-//            break;
-//        case 0:
-//            /* This is processed by the child */
-//            execv ("Worker", my_args);
-//            puts("Uh oh! If this prints, execv() must have failed");
-//            exit(EXIT_FAILURE);
-//            break;
-//        default:
-//            /* This is processed by the parent */
-//            puts ("This is a message from the parent");
-//            break;
-//    }
-//
-//    puts ("End of parent program");
-//    return 0;
-//}
-//
-///*
-// * Program output:
-// fork()ing
-// This is a message from the parent
-// End of parent program
-// I am the child
-// Arg 1 arg1
-// Arg 2 arg2
-// *
-// */
