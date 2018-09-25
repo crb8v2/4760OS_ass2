1276c3e7a671636b16ef8fc072f6766bd8aca1de
Connor <connor@admin.com>
2018-09-25 16:29:19 -0500

    shared memory works and passes n to worker
    to do:
    	implement clock

--- worker.c
+++ worker.c
@@ -1,7 +1,28 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHMKEY 123123
+#define BUFF_SZ sizeof(int)
 
 int main(void) {
 
-    printf("hello world\n");
+    //shared memory
+    int shmid = shmget ( SHMKEY, BUFF_SZ, 0777 | IPC_CREAT );
+    if ( shmid == -1 )
+    {
+        printf("Error in shared memory");
+        exit (1);
+    }
+    // pointer to shared memory
+    int *cint = ( int * )( shmat ( shmid, 0, 0 ) );
 
+    printf("n is %d\n", *cint);
+
+    sleep(2);
+
+    return 0;
 }
